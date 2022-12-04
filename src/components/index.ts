966de2e4e85f62ba6652d03a84dc7fd7617e6561
guxuerui <im.guxuerui@gmail.com>
2022-12-04 11:52:07 +0800

    fix(index.ts): delete unused property

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 import PrettyTimeline from './PrettyTimeline.vue' // 引入封装好的组件
 
 export default {
-  install: (app: any, options: any): void => {
+  install: (app: any): void => {
     app.component('PrettyTimeline', PrettyTimeline)
   },
 }
