f3738bf40ac9b103803e62f9285ab5012952ea10
guxuerui <im.guxuerui@gmail.com>
2022-12-06 16:35:41 +0800

    fix(isMobile.ts): use function resolveRef

--- src/composables/isMobile.ts
+++ src/composables/isMobile.ts
@@ -1,9 +1,10 @@
+import { resolveRef } from '@vueuse/core'
+
 export function isMobile() {
-  return computed(() => {
+  return resolveRef(() => {
     // return navigator.userAgent.match(
     //   /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i,
     // )
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
   })
 }
-
